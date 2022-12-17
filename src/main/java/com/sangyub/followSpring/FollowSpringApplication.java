2e90e2f30524785fde1027c460cc704048e9988f
Sangyub Ji <sangyubji@Sangyubui-MacBookAir.local>
2022-12-18 01:33:09 +0900

    Initial commit

---
+++ src/main/java/com/sangyub/followSpring/FollowSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sangyub.followSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowSpringApplication.class, args);
+	}
+
+}
